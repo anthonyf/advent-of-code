23b30d357fdd111fe1a85668aeb018a60ee075d4
Anthony Fairchild <anthony@Anthonys-MacBook-Pro.local>
2016-12-04 10:34:32 -0600

    day 4

---
+++ src/advent_of_code/day4.2.clj
@@ -0,0 +1,48 @@
+(ns advent-of-code.day4.2
+  (:require [advent-of-code.day4.1 :as day4.1]))
+
+(defn rotate-char
+  [c]
+  (case c
+    \space c
+    \- \space
+    (char (+ (mod (- (+ (int c) 1)
+                     (int \a))
+                  26)
+             (int \a)))))
+
+#_ (rotate-char \-)
+
+(defn shift-name
+  [name]
+  (apply str (map rotate-char
+                  name)))
+
+#_ (shift-name "ab-vz")
+
+(defn decrypt-room
+  [{:keys [name sector-id _] :as line}]
+  (assoc line :decrypted (reduce (fn [name _]
+                                   (shift-name name))
+                                 name
+                                 (range sector-id))))
+
+#_ (decrypt-room (day4.1/parse-line "qzmt-zixmtkozy-ivhz-343[asdsf]"))
+
+
+(defn solve []
+  (reduce (fn [answer {:keys [decrypted sector-id]}]
+            (if (= decrypted "northpole object storage ")
+              (reduced sector-id)
+              nil))
+          nil
+          (map decrypt-room
+               (reduce (fn [valid {:keys [name sector-id checksum] :as line}]
+                         (if (day4.1/valid-checksum? line)
+                           (conj valid line)
+                           valid))
+                       []
+                       day4.1/data))))
+
+#_ (solve)
+;; => 501
