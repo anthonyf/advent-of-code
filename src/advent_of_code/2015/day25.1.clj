bf05e327c689792e8cfafda614a52b44e2dbd97e
Anthony Fairchild <anthony@Anthonys-MBP.home>
2016-12-05 09:10:13 -0500

    2015 day1.1

--- src/advent_of_code/2015/day25.1.clj
+++ src/advent_of_code/2015/day25.1.clj
@@ -25,3 +25,40 @@
 ;; => 1
 #_ (inc (.indexOf (indexes) [14 30]))
 ;; => 933
+#_ (.indexOf (indexes)
+             [2978 3083])
+;; => 18361852
+
+(defn code
+  [n]
+  (loop [i 1
+         code 20151125]
+    (if (= n i)
+      code
+      (recur (inc i)
+             (rem (* code 252533)
+                  33554393)))))
+
+#_ (code 1) ;; => 20151125
+#_ (code 2) ;; => 31916031
+#_ (code 3) ;; => 18749137
+
+(defn solve [row col]
+  (code
+   (inc (.indexOf (indexes)
+                  [row col]))))
+
+
+#_ (solve 2 1)
+;; => 31916031
+
+#_ (solve 6 4)
+;; => 24659492
+
+#_ (solve 6 6)
+;; => 27995004
+
+;; To continue, please consult the code grid in the manual.  Enter the code at
+;; row 2978, column 3083.
+#_ (solve 2978 3083)
+;; => 2650453
