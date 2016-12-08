b9aa770708a5dd354f6b03bbc84f49e349984e1d
Anthony Fairchild <anthony@Anthonys-MBP.home>
2016-12-08 10:36:03 -0500

    day 8.1 finished

--- src/advent_of_code/2016/day8_1.clj
+++ src/advent_of_code/2016/day8_1.clj
@@ -21,6 +21,87 @@
 #_ (parse-command "rotate column x=0 by 10")
 ;; => [:rotate-column 0 10]
 
+(defn rect
+  [board w h]
+  (reduce (fn [board x]
+            (reduce (fn [board y]
+                      (assoc board [(mod x 50)
+                                    (mod y 6)] :#))
+                    board
+                    (range h)))
+          board
+          (range w)))
+
+#_ (rect {} 3 2)
+;; => {[0 0] :#, [0 1] :#, [1 0] :#, [1 1] :#, [2 0] :#, [2 1] :#}
 
 
-#_ (map parse-command (str/split-lines d/raw-data))
+(defn print-board
+  [board]
+  (dotimes [y 6]
+    (dotimes [x 50]
+      (print (case (board [x y])
+               :# "#"
+               ".")))
+    (println)))
+
+#_ (print-board (rect {} 3 2))
+
+(defn rotate
+  [row n]
+  (vec (reduce (fn [row _]
+                 (concat [(last row)]
+                         (take (- (count row) 1) row)))
+               row
+               (range n))))
+
+#_ (rotate [1 2 3 4] 2)
+;; => [3 4 1 2]
+
+(defn rotate-row
+  [board y spaces]
+  (let [row (map (fn [x]
+                   (board [x y]))
+                 (range 50))
+        row (rotate row spaces)]
+    (reduce (fn [board x]
+              (assoc board [x y] (row x)))
+            board
+            (range 50))))
+
+#_ (print-board (rotate-row (rect {} 3 2) 0 51))
+
+(defn rotate-column
+  [board x spaces]
+  (let [col (map (fn [y]
+                   (board [x y]))
+                 (range 6))
+        col (rotate col spaces)]
+    (reduce (fn [board y]
+              (assoc board [x y] (col y)))
+            board
+            (range 6))))
+
+#_ (print-board (rotate-column (rect {} 3 2) 0 7))
+
+
+(defn solve
+  []
+  (let [board (reduce (fn [board [command a b]]
+                        (case command
+                          :rect (rect board a b)
+                          :rotate-row (rotate-row board a b)
+                          :rotate-column (rotate-column board a b)))
+                      {}
+                      (map parse-command (str/split-lines d/raw-data)))]
+    (reduce (fn [sum x]
+              (reduce (fn [sum y]
+                        (if (= :# (board [x y]))
+                          (+ sum 1)
+                          sum))
+                      sum
+                      (range 6)))
+            0
+            (range 50))))
+
+#_ (solve);; => 121
