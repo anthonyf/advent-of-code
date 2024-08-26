923de4b6b7bb834e99222e43c915514eb1ddad16
Anthony Fairchild <anthony.fairchild@tapad.com>
2024-08-26 14:24:56 -0400

    2023 day 7 part 1 WIP

--- 2023/day-07.lisp
+++ 2023/day-07.lisp
@@ -3,7 +3,18 @@
   (:mix #:advent-of-code/util
 	#:iterate
 	#:cl-ppcre
-	#:trivia
-	#:arrow-macros))
+	#:trivia))
 
 (in-package #:advent-of-code/2023/day-07)
+
+(defun parse-file (file)
+  (serapeum:~>>
+   file
+   (file-lines)
+   (mapcar (lambda (line)
+	     (let-match (((list cards score) (split "\\s" line)))
+	       (list :cards cards :score (parse-integer score)))))))
+
+
+#+nil
+(parse-file "2023/day-07-sample.txt")
