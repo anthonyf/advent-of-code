da08528a5c4d3d1e09f723c375e32d1188a4b353
Anthony Fairchild <anthony.fairchild@tapad.com>
2024-11-20 14:39:57 -0500

    2023 day 7 part 2 complete

--- 2023/day-07.lisp
+++ 2023/day-07.lisp
@@ -2,6 +2,7 @@
   (:use #:cl)
   (:mix #:advent-of-code/util
 	#:iterate
+	#:arrow-macros
 	#:serapeum
 	#:cl-ppcre
 	#:trivia))
@@ -26,10 +27,10 @@ QQQJA 483")))
 
 (defparameter *hands* (reverse (vector :five-of-a-kind :four-of-a-kind :full-house :three-of-a-kind :two-pair :one-pair :high-card)))
 
-(defparameter *cards* (reverse (vector #\A #\K #\Q #\J #\T #\9 #\8 #\7 #\6 #\5 #\4 #\3 #\2)))
+(defparameter *all-cards* (reverse (vector #\A #\K #\Q #\J #\T #\9 #\8 #\7 #\6 #\5 #\4 #\3 #\2)))
 
 (defun hand-type (cards)
-  (let* ((groups (sort (mapcar #'length (assort cards)) #'<)))
+  (let ((groups (sort (mapcar #'length (assort cards)) #'<)))
     (ematch groups
       ((list 5) :five-of-a-kind)
       ((list 1 4) :four-of-a-kind)
@@ -40,22 +41,22 @@ QQQJA 483")))
       ((list 1 1 1 1 1) :high-card))))
 
 (defun hand-score (hand-type cards)
-  (let ((base (length *cards*)))
+  (let ((base (length *all-cards*)))
     (+ (* (position hand-type *hands*)
 	  (expt base 5))
        (iter
 	 (for i from 4 downto 0)
 	 (for card in-vector cards)
-	 (for p = (position card *cards*))
+	 (for p = (position card *all-cards*))
 	 (for d = (* p (expt base i)))
 	 (sum d)))))
 
 #+nil
 (hand-score :two-pair "KTJJT")
 
-(defun solve-1 (input)
+(defun solve-1 (input &optional (hand-type-fun #'hand-type))
   (let* ((hands (iter (for (cards . bid) in input)
-		  (collect (list (hand-type cards) cards bid))))
+		  (collect (list (funcall hand-type-fun cards) cards bid))))
 	 (hands (iter (for (hand-type cards bid) in hands )
 		  (collect (list (hand-score hand-type cards) hand-type cards bid))))
 	 (hands-sorted (sort hands #'< :key #'car)))
@@ -70,3 +71,38 @@ QQQJA 483")))
 
 #+nil
 (solve-1 *input*)
+
+
+(defun most-common-card (cards)
+  (let ((cards (remove #\J cards)))
+    (if (= 0 (length cards))
+	#\K ;; doesn't matter, return some valid card
+	(-<> cards
+	  assort
+	  (stable-sort #'> :key #'length)
+	  first
+	  (elt 0)))))
+
+#+nil
+(most-common-card "jabcd")
+
+(string-replace-all (string #\J) "123J4" (string (most-common-card "123J4")))
+
+(defun hand-type-2 (cards)
+  (hand-type (if (string-contains-p "j" cards)
+		 (let* ((j-replacement (most-common-card cards)))
+		   (string-replace-all (string #\J) cards  (string j-replacement)))
+		 cards)))
+
+#+nil
+(hand-type-2 "J22J4")
+
+(defun solve-2 (input)
+  (let ((*all-cards* (reverse (vector #\A #\K #\Q #\T #\9 #\8 #\7 #\6 #\5 #\4 #\3 #\2 #\J))))
+    (solve-1 input #'hand-type-2)))
+
+#+nil
+(solve-2 *sample*)
+
+#+nil
+(solve-2 *input*)
