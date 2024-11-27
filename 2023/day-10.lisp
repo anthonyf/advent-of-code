47533e7de94029376c44d749be0976ff029c9bb0
Anthony Fairchild <fairchild.anthony@gmail.com>
2024-11-26 19:44:02 -0500

    switch day 10 to CL

--- 2023/day-10.lisp
+++ 2023/day-10.lisp
@@ -1,29 +1,40 @@
 (uiop:define-package #:advent-of-code/2023/day-10
-  (:use #:coalton
-	#:coalton-prelude)
-  (:local-nicknames (#:u #:aoc/coalton-util)))
+  (:use #:cl)
+  (:mix #:advent-of-code/util
+	#:iterate)
+  )
 
 (in-package #:advent-of-code/2023/day-10)
 
-(named-readtables:in-readtable coalton:coalton)
+(defparameter *pipes* '((#\| (0 -1)  (0 1)) ;;is a vertical pipe connecting north and south.
+			(#\- (1  0) (-1 0)) ;;is a horizontal pipe connecting east and west.
+			(#\L (0 -1)  (1 0)) ;;is a 90-degree bend connecting north and east.
+			(#\J (0 -1) (-1 0)) ;;is a 90-degree bend connecting north and west.
+			(#\7 (0  1) (-1 0)) ;;is a 90-degree bend connecting south and west.
+			(#\F (0  1)  (1 0)) ;;is a 90-degree bend connecting south and east.
+			(#\. ) ;; is ground ; there is no pipe in this tile.
+			(#\S) ;; is the starting position of the animal ; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+			))
 
-(coalton-toplevel
-
-  (define input (u:input-lines 2023 10))
-
-  (define sample (u:string-lines ".....
+(defparameter *sample* (string-lines ".....
 .S-7.
 .|.|.
 .L-J.
 ....."))
 
-  (define (start-position input)
-    (coalton-library/string:chars ))
-  
-  (define (solve-1 _input)
-    nil)
-  )
+(defun starting-position (input)
+  (iter
+    (for y from 0)
+    (for line in input)
+    (for x = (position #\S line))
+    (finding (cons x y) such-that x)))
 
 #+nil
-(coalton sample)
+(starting-position *sample*)
+
+
+;;(defun step (input n ))
+
+(defun solve-1 (input)
+  )
 
