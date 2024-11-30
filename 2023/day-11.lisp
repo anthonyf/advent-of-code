a681b53de5c9b8088a0cce04617e34b3c79d5b1d
Anthony Fairchild <fairchild.anthony@gmail.com>
2024-11-29 23:30:51 -0500

    2023 day 11 part 1

--- 2023/day-11.lisp
+++ 2023/day-11.lisp
@@ -1,12 +1,11 @@
 (uiop:define-package #:aoc/2023/day-11
   (:use #:cl)
-  (:mix #:aoc/util
-	#:iterate)
+  (:mix #:aoc/util)
   )
 
 (in-package #:aoc/2023/day-11)
 
-(defparameter *sample* (string-lines "...#......
+(defparameter *sample* (coerce  (string-lines "...#......
 .......#..
 #.........
 ..........
@@ -15,22 +14,40 @@
 .........#
 ..........
 .......#..
-#...#....."))
+#...#.....")
+				'vector))
 
-(iter (for y from 0 below (length *sample*))
-  (collect y result-type vector))
+(defparameter *input* (coerce (input-lines 2023 11) 'vector))
 
-(defun galaxy-positions (input)
-  (iter (for y from 0 below (length input))
-    (for line = (svref input y))
-    (for exp-y = 0)
-    (if (iter (for char in-vector line)
-	  (always (eql #\. char)))
-	(incf exp-y)
-	
-	(appending (iter (for x from 0 below (length line))
-		     (when (eql #\# char)
-		       (collect (cons x (+ y exp-y)))))))))
+(defun solve-1 (input)
+  (let* ((y-expantions (coerce (loop for y from 0 below (length input)
+				     with exp = 0
+				     if (loop for x from 0 below (length (aref input 0))
+					      always (eql #\. (aref (aref input y) x)))
+				       collect (incf exp)
+				     else
+				       collect exp)
+			       'vector))
+	 (x-expansions (coerce (loop for x from 0 below (length (aref input 0))
+				     with exp = 0
+				     if (loop for y from 0 below (length input)
+					      always (eql #\. (aref (aref input y) x)))
+				       collect (incf exp)
+				     else
+				       collect exp)
+			       'vector))
+	 (galaxy-positions (loop for y from 0 below (length input)
+				 appending (loop for x from 0 below (length (aref input 0))
+						 when (eql #\# (aref (aref input y) x))
+						   collect (cons (+ x (aref x-expansions x))
+								 (+ y (aref y-expantions y)))))))
+    (loop for ((x1 . y1) . rest) on galaxy-positions
+	  sum (loop for (x2 . y2) in rest
+		    sum (+ (abs (- x1 x2)) (abs (- y1 y2)))))
+    ))
 
 #+nil
-(galaxy-positions *sample*)
+(solve-1 *sample*)
+
+#+nil
+(solve-1 *input*)
