241b9d3a2c53ee791a8741163f423226e2f46620
Anthony Fairchild <fairchild.anthony@gmail.com>
2024-12-01 18:38:53 -0500

    2023 day 12 part 1

--- 2023/day-12.lisp
+++ 2023/day-12.lisp
@@ -1,17 +1,10 @@
 (uiop:define-package #:aoc/2023/day-12
   (:use #:cl)
   (:mix #:aoc/util
-	#:arrow-macros))
+	#:arrow-macros
+	#:trivia))
 
 (in-package #:aoc/2023/day-12)
-
-(defparameter *sample* (string-lines "???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1"))
-
 
 (defun parse-line (line)
   (destructuring-bind (pattern contiguous-damaged)
@@ -20,37 +13,53 @@
 			  (uiop:split-string contiguous-damaged
 					     :separator '(#\,))))))
 
-
 (defun parse-lines (lines)
   (mapcar #'parse-line lines))
 
+(defparameter *sample* (parse-lines (string-lines "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1")))
+
+(defparameter *input* (parse-lines (input-lines 2023 12)))
+
+(defun count-arrangements (records damaged-counts &optional (current-count 0))
+  (match (list records damaged-counts)
+    ((list (vector) (list))
+     1)
+    ((guard (list (vector) (list d))
+	    (= current-count d))
+     1)
+    ((list (vector* #\.) (list))
+     (count-arrangements (subseq records 1) damaged-counts current-count))
+    ((guard (list (vector* #\.) (list* d _))
+	    (or (= current-count d)
+		(= current-count 0)))
+     (count-arrangements (subseq records 1) (if (= current-count d)
+						(rest damaged-counts)
+						damaged-counts) 0))
+    ((guard (list (vector* #\#) (list* d _))
+	    (< current-count d))
+     (count-arrangements (subseq records 1) damaged-counts (1+ current-count)))
+    ((list (vector* #\?) _)
+     (+ (count-arrangements (concatenate 'string "#" (subseq records 1))
+			    damaged-counts current-count)
+	(count-arrangements (concatenate 'string "." (subseq records 1))
+			    damaged-counts current-count)))
+    (_ 0)))
+
 #+nil
-(parse-lines *sample*)
+(count-arrangements "????.######..#####." (list 1 6 5))
 
-(concatenate 'string "b" (subseq "hello" 1))
-
-(defun count-arrangements (records damaged-counts)
-  (let ()
-    (cond
-      ((and (= 0 (length records))
-	    (null damaged-counts))
-       1)
-      
-      ((or (= 0 (length records))
-	   (null damaged-counts))
-       0)
-      ((eql #\. (aref records 0))
-       (count-arrangements (subseq records 1) damaged-counts))
-      ((eql #\# (aref records 0))
-       (count-arrangements (subseq records 1) (if (= 1 (first damaged-counts))
-						  (rest damaged-counts)
-						  (cons (1- (first damaged-counts))
-							(rest damaged-counts)))))
-      ((eql #\? (aref records 0))
-       (+ (count-arrangements (concatenate 'string "." (subseq records 1))
-			      damaged-counts)
-	  (count-arrangements (concatenate 'string "#" (subseq records 1))
-			      damaged-counts))))))
+(defun solve-1 (input)
+  (loop for (records damaged-counts) in input
+	sum (count-arrangements records damaged-counts)))
 
 #+nil
-(count-arrangements "???.###" '(1 1 3))
+(solve-1 *sample*)
+
+#+nil
+(solve-1 *input*)
+
--- advent-of-code.asd
+++ advent-of-code.asd
@@ -3,31 +3,34 @@
 #-asdf3.1 (error "LIL requires ASDF 3.1 or later. Please upgrade your ASDF.")
 
 (asdf:defsystem #:advent-of-code
-  :description "Describe advent-of-code here"
-  :author "Your Name <your.name@example.com>"
-  :license  "Specify license here"
-  :version "0.0.1"
-  :depends-on (#:cl-dotenv
-	       #:coalton
-	       #:alexandria
-	       #:serapeum
-	       #:trivia
-	       #:arrow-macros
-	       #:cl-ppcre)
-  :serial t
-  :components ((:file "util")
-	       (:file "coalton-util")
-	       (:module "2023"
-		:components ((:file "day-01") ;; **
-			     (:file "day-02") ;; **
-			     (:file "day-03") ;; **
-			     (:file "day-04") ;; **
-			     (:file "day-05") ;; **
-			     (:file "day-06") ;; **
-			     (:file "day-07") ;; **
-			     (:file "day-08") ;; *
-			     (:file "day-09") ;; **
-			     (:file "day-10") ;;
-			     (:file "day-11") ;; **
-			     ;;(:file  "day-12") ;;
-			     ))))
+		:description "Describe advent-of-code here"
+		:author "Your Name <your.name@example.com>"
+		:license  "Specify license here"
+		:version "0.0.1"
+		:depends-on (#:cl-dotenv
+			     #:coalton
+			     #:alexandria
+			     #:serapeum
+			     #:trivia
+			     #:arrow-macros
+			     #:cl-ppcre)
+		:serial t
+		:components ((:file "util")
+			     (:file "coalton-util")
+			     (:module "2023"
+				      :components ((:file "day-01") ;; **
+						   (:file "day-02") ;; **
+						   (:file "day-03") ;; **
+						   (:file "day-04") ;; **
+						   (:file "day-05") ;; **
+						   (:file "day-06") ;; **
+						   (:file "day-07") ;; **
+						   (:file "day-08") ;; *
+						   (:file "day-09") ;; **
+						   (:file "day-10") ;;
+						   (:file "day-11") ;; **
+						   (:file "day-12") ;; *
+						   ))
+			     (:module "2024"
+				      :components ((:file "day-01") ;; **
+						   ))))
