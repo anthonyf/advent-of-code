9d4b687914c76ce01875ebbf81f0ea03250e2b40
Anthony Fairchild <fairchild.anthony@gmail.com>
2024-12-04 22:39:52 -0500

    placeholders

---
+++ 2024/day-06.lisp
@@ -0,0 +1,5 @@
+(uiop:define-package #:aoc/2024/day-06
+  (:use #:cl)
+  (:mix #:aoc/util))
+
+(in-package #:aoc/2024/day-06)
---
+++ 2024/day-07.lisp
@@ -0,0 +1,5 @@
+(uiop:define-package #:aoc/2024/day-07
+  (:use #:cl)
+  (:mix #:aoc/util))
+
+(in-package #:aoc/2024/day-07)
